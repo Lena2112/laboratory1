b6492a5664d72a288690e31f2420ef888a923c7c
Lena2112 <Helen.MONSTRIK@gmail.com>
2016-09-13 16:07:47 +0300

    replacing tabs with spaces

--- jump/jump/jump.cpp
+++ jump/jump/jump.cpp
@@ -16,70 +16,70 @@
 
 int GetTheMaximumJumpHeight()
 {
-	int theMaximumJumpHeight;
-	bool flag = false;
-	while (!flag)
-	{
-	    printf("The maximum jump height: ");
-	    if (0 == scanf("%d", &theMaximumJumpHeight))
-	    {
-		    printf("\nexpected floating-point number\n");
-		    exit(1);
-	    }
-		if (theMaximumJumpHeight > 0 && theMaximumJumpHeight < INT_MAX)
-		{
-			flag = true;
-		}
-		else
-		{
-			printf("value must be greater than zero" "\n");
-		}
-	}
-	return theMaximumJumpHeight;
+    int theMaximumJumpHeight;
+    bool flag = false;
+    while (!flag)
+    {
+        printf("The maximum jump height: ");
+        if (0 == scanf("%d", &theMaximumJumpHeight))
+        {
+            printf("\nexpected floating-point number\n");
+            exit(1);
+        }
+        if (theMaximumJumpHeight > 0 && theMaximumJumpHeight < INT_MAX)
+        {
+            flag = true;
+        }
+        else
+        {
+            printf("value must be greater than zero" "\n");
+        }
+    }
+    return theMaximumJumpHeight;
 }
 
 void DisplayStateAtTheMoment(const float currentTimePoint, const float v0, const float g)
 {
-	double jumpHeight = v0 * currentTimePoint - 0.5 * g * currentTimePoint * currentTimePoint;
-	if (jumpHeight >= 0)
-	{
-		printf("Time=%f, jump height=%f\n", currentTimePoint, jumpHeight);
-	}
-	return;
+    double jumpHeight = v0 * currentTimePoint - 0.5 * g * currentTimePoint * currentTimePoint;
+    if (jumpHeight >= 0)
+    {
+        printf("Time=%f, jump height=%f\n", currentTimePoint, jumpHeight);
+    }
+    return;
 }
 
 void OutputOnDisplay(const float timeWhenTheMaximumHeight, const float g)
 {
-	printf("Time when the maximum height:%f\n", timeWhenTheMaximumHeight);
-	float v0 = g * timeWhenTheMaximumHeight;
-	bool passedHalfWay = false;
-	for (float currentTimePoint = 0; currentTimePoint < timeWhenTheMaximumHeight * 2; currentTimePoint += 0.1f)
-	{
-		if (currentTimePoint > timeWhenTheMaximumHeight && !passedHalfWay)
-		{
-			passedHalfWay = true;
-			DisplayStateAtTheMoment(timeWhenTheMaximumHeight, v0, g);
-		}
-		DisplayStateAtTheMoment(currentTimePoint, v0, g);
-	}
-	DisplayStateAtTheMoment(timeWhenTheMaximumHeight * 2, v0, g);
-	return;
+    printf("Time when the maximum height:%f\n", timeWhenTheMaximumHeight);
+    float v0 = g * timeWhenTheMaximumHeight;
+    bool passedHalfWay = false;
+    for (float currentTimePoint = 0; currentTimePoint < timeWhenTheMaximumHeight * 2; currentTimePoint += 0.1f)
+    {
+        if (currentTimePoint > timeWhenTheMaximumHeight && !passedHalfWay)
+        {
+            passedHalfWay = true;
+            DisplayStateAtTheMoment(timeWhenTheMaximumHeight, v0, g);
+        }
+        DisplayStateAtTheMoment(currentTimePoint, v0, g);
+    }
+    DisplayStateAtTheMoment(timeWhenTheMaximumHeight * 2, v0, g);
+    return;
 }
 
 void PpressingExpectation()
 {
-	printf("To exit, press any key");
-	getch();
+    printf("To exit, press any key");
+    getch();
 }
 
 int main(int, char *[])
 {
     const float g = 9.8f;
-	int theMaximumJumpHeight = GetTheMaximumJumpHeight();
-	float timeWhenTheMaximumHeight = sqrt(theMaximumJumpHeight * 2 / g);
-	OutputOnDisplay(timeWhenTheMaximumHeight, g);	
+    int theMaximumJumpHeight = GetTheMaximumJumpHeight();
+    float timeWhenTheMaximumHeight = sqrt(theMaximumJumpHeight * 2 / g);
+    OutputOnDisplay(timeWhenTheMaximumHeight, g);    
 
-	PpressingExpectation();
-	return 0;
+    PpressingExpectation();
+    return 0;
 }
 
